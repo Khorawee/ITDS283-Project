703c66b08c5c47614f21e853ec41f678e1f9ef58
unknown <khorawee.152@gmail.com>
2026-04-19 09:45:39 +0700

    LearnFlow API - Ready for deployment

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-cd /app/learnflow_api || cd learnflow_api
-pip install -r requirements.txt
+cd /app/learnflow_api || cd learnflow_api || exit 1
 gunicorn -w 4 -b 0.0.0.0:$PORT "app:create_app()"
